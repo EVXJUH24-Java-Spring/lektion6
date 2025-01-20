8b833b8f38898b152dc35f800eca1b6d55c2b4cc
EnanderW <williamenander@gmail.com>
2025-01-20 11:00:03 +0100

    add comments

--- src/main/java/se/deved/lektion6/AuthenticationFilter.java
+++ src/main/java/se/deved/lektion6/AuthenticationFilter.java
@@ -22,31 +22,47 @@ public class AuthenticationFilter extends OncePerRequestFilter {
 
     private final UserRepository userRepository;
 
+    // Metoden körs innan varje request
     @Override
     protected void doFilterInternal(
+            // Innehåller information om requesten som headers och body
             HttpServletRequest request,
+            // Innehåller information om responsen som skall skickas, men vi använder den inte
             HttpServletResponse response,
+            // Innehåller en lista på alla andra filter som finns i Spring
+            // Vi använder den endast för att gå vidare i "kedjan" när vårat filter är färdigt
             FilterChain filterChain
     ) throws ServletException, IOException {
+        // Hämta ut token värdet från "Authorization" headern.
+        // Om den är null ignorerar vi filtret och går till nästa filter.
+        // Om en endpoint kräver autentisering men ingen token kommer in, kommer Spring i ett senare filter blocka detta.
+        // Vi blockar inte åtkomst själva.
         String authHeader = request.getHeader("Authorization");
         if (authHeader == null || authHeader.isBlank()) {
             filterChain.doFilter(request, response);
             return;
         }
 
+        // JWT kräver en algoritm för att kunna kryptera tokens
         Algorithm algorithm = Algorithm.HMAC256("secretnooneshallsee");
+        // För att verifiera tokens måste man skala en "JWTVerifier" som innehåller en 'verify' metod.
         JWTVerifier verifier = JWT.require(algorithm).withIssuer("auth0").build();
 
+        // Verifiera token värdet från användaren och hämta ut användarens id
         DecodedJWT jwt = verifier.verify(authHeader);
         UUID userId = UUID.fromString(jwt.getSubject());
 
+        // Se om det finns en användare i databasen med id:t
         User user = userRepository.findById(userId).orElseThrow();
 
-        // Nu är vi autentiserade
+        // Om det finns en användare så har den autentiserat sig och då måste vi informera Spring om det
+        // Denna rad säger åt Spring att användaren är inloggad och kan fortsätta till endpointen.
         SecurityContextHolder.getContext().setAuthentication(new UsernamePasswordAuthenticationToken(
                 user, user.getPassword(), new ArrayList<>()
         ));
 
+        // När har autentiserat hos vill vi gå till nästa filter och till slut endpointen.
+        // Denna rad anropar nästa filter i kedjan (antagligen inbyggda Spring filter).
         filterChain.doFilter(request, response);
     }
 }
--- src/main/java/se/deved/lektion6/SecurityConfig.java
+++ src/main/java/se/deved/lektion6/SecurityConfig.java
@@ -10,24 +10,40 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-@Configuration
-@EnableWebSecurity
+// Denna klass definierar de beans (dependencies) som Spring behöver för
+// att kunna konfigurera säkerhetsinställningar (som endpoint authentication exempelvis).
+@Configuration // Denna gör det möjligt att skapa beans genom @Bean metoder
+@EnableWebSecurity // Denna säger åt Spring att vi vill konfigurerar säkerhet i denna klass
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http, UserRepository userRepository) throws Exception {
+        // Vi måste stänga av CSRF
+        // Mer om detta i nästa kurs
         http.csrf(AbstractHttpConfigurer::disable)
+                // Innehållet i denna metod konfigurerar
+                // vilka endpoints som skall kräva autentisering
                 .authorizeHttpRequests(auth -> {
                     auth
+                            // requestMatches säger vilken endpoint som vi skall applicera säkerhet på
+                            // permitAll innebär att man inte måste autentisera sig
                             .requestMatchers("/register").permitAll()
                             .requestMatchers("/login").permitAll()
+                            // authenticated innebär att man måste autentisera sig
                             .requestMatchers("/test").authenticated();
                 })
+                // För att kunna autentisera användare måste vi kunna verifera JWT tokens.
+                // Det kan göras genom ett filter som körs för varje request.
+                // Här registrerar vi detta filter innan ett av Springs egna filter som vi inte vill använda,
+                // och därför lägger vi vårat filter innan deras filter.
                 .addFilterBefore(new AuthenticationFilter(userRepository), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
 
+    // PasswordEncoder är ett inbyggt interface med funktioner för password hashing
+    // Vi kan använda det genom att definiera en bean med exempelvis BCrypt.
+    // Se UserController för användning i register och login.
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
